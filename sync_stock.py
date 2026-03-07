7e36fa3e2a41ddbb262dc5faef853b0c27add837
Dominus09 <katoonxd@gmail.com>
2026-03-07 15:38:47 -0300

    Update sync_stock.py

--- sync_stock.py
+++ sync_stock.py
@@ -5,6 +5,7 @@ import time
 print("SYNC STOCK")
 
 BASE="https://api.bsale.io/v1"
+NOCODB="https://db.quillotana.cl"
 
 TOKEN=os.getenv("BSALE_TOKEN_Mini")
 NOCOTOKEN=os.getenv("NocoDB_token")
@@ -16,53 +17,54 @@ HEADNOCO={
 "Content-Type":"application/json"
 }
 
-NOCODB="https://db.quillotana.cl"
-
 LIMIT=50
 
 TABLE_STOCK="mxs2lyz86cnxd23"
 
 def api(url,params=None):
 
- while True:
+    while True:
 
-  r=requests.get(url,headers=HEAD,params=params)
+        r=requests.get(url,headers=HEAD,params=params)
 
-  if r.status_code==429:
+        if r.status_code==429:
 
-   retry=int(r.json().get("retry_after",60))
-   print("RATE LIMIT",retry)
-   time.sleep(retry)
-   continue
+            retry=int(r.json().get("retry_after",60))
+            print("RATE LIMIT",retry)
+            time.sleep(retry)
+            continue
 
-  r.raise_for_status()
-  time.sleep(0.2)
+        r.raise_for_status()
+        time.sleep(0.15)
 
-  return r.json()
+        return r.json()
 
 offset=0
 
 while True:
 
- j=api(f"{BASE}/stocks.json",{"limit":LIMIT,"offset":offset})
+    j=api(
+        f"{BASE}/stocks.json",
+        {"limit":LIMIT,"offset":offset}
+    )
 
- items=j.get("items",[])
+    items=j.get("items",[])
 
- if not items:
-  break
+    if not items:
+        break
 
- for s in items:
+    for s in items:
 
-  requests.post(
-  f"{NOCODB}/api/v2/tables/{TABLE_STOCK}/records",
-  json={
-  "variant_id":s["variant"]["id"],
-  "office_id":s["office"]["id"],
-  "quantity_available":s["quantityAvailable"]
-  },
-  headers=HEADNOCO
-  )
+        requests.post(
+            f"{NOCODB}/api/v2/tables/{TABLE_STOCK}/records",
+            json={
+            "variant_id":s["variant"]["id"],
+            "office_id":s["office"]["id"],
+            "quantity_available":s["quantityAvailable"]
+            },
+            headers=HEADNOCO
+        )
 
- offset+=LIMIT
+    offset+=LIMIT
 
 print("STOCK DONE")
