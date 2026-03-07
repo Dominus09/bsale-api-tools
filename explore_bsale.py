d9c1251ea50919b543b4778cd6ee170fb78f4f93
Dominus09 <katoonxd@gmail.com>
2026-03-06 21:46:52 -0300

    Update explore_bsale.py

--- explore_bsale.py
+++ explore_bsale.py
@@ -4,7 +4,7 @@ headers = {
  "access_token": os.getenv("BSALE_TOKEN_Mini")
 }
 
-url = "https://api.bsale.io/v1/price_lists/details.json?limit=5"
+url = "https://api.bsale.io/v1/price_lists.json?expand=[details]&limit=50"
 
 r = requests.get(url, headers=headers)
 
