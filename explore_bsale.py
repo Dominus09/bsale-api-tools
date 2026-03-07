b6242c98317a42da775f3a02ebd12ccfd9888c99
Dominus09 <katoonxd@gmail.com>
2026-03-06 21:39:44 -0300

    Update explore_bsale.py

--- explore_bsale.py
+++ explore_bsale.py
@@ -1,28 +1,11 @@
-import requests
-import os
-import json
-
-BSALE_TOKEN = os.getenv("BSALE_TOKEN_Mini")
+import requests, os, json
 
 headers = {
-    "access_token": BSALE_TOKEN
+ "access_token": os.getenv("BSALE_TOKEN_Mini")
 }
 
-variant_id = 5547
+url = "https://api.bsale.io/v1/price_lists/1/details.json"
 
-print("TEST COSTS")
-
-costs_url = f"https://api.bsale.io/v1/variants/{variant_id}/costs.json"
-
-r = requests.get(costs_url, headers=headers)
-
-print(json.dumps(r.json(), indent=2))
-
-
-print("\nTEST PRICES")
-
-prices_url = f"https://api.bsale.io/v1/price_lists/details.json?variantid={variant_id}"
-
-r = requests.get(prices_url, headers=headers)
+r = requests.get(url, headers=headers)
 
 print(json.dumps(r.json(), indent=2))
