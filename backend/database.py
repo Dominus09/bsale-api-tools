bd7c97036b34aef261baa19af260ee6c393297b4
Dominus09 <katoonxd@gmail.com>
2026-03-11 22:17:46 -0300

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,20 @@
+import os
+import requests
 
+NOCODB_URL = os.getenv("NOCODB_URL", "https://db.quillotana.cl")
+NOCODB_TOKEN = "R3EhSD8si-WSVdsPxlQVGAfiHRRcDR9cHGHJdBJL"
+
+HEADERS = {
+    "xc-token": NOCODB_TOKEN
+}
+
+
+def noco_get(table_id, params=None):
+
+    url = f"{NOCODB_URL}/api/v2/tables/{table_id}/records"
+
+    r = requests.get(url, headers=HEADERS, params=params)
+
+    r.raise_for_status()
+
+    return r.json().get("list", [])
